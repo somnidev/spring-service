62552900b4a3b482e72c1ec81ad3215306fff7e5
Simon Oser <simon.oser@gmail.com>
2024-06-26 21:49:52 +0200

    Adds ApplicationModuleListener annotation for async method call

--- src/main/java/dev/smo/spring/example/service/inventory/Inventory.java
+++ src/main/java/dev/smo/spring/example/service/inventory/Inventory.java
@@ -1,13 +1,17 @@
 package dev.smo.spring.example.service.inventory;
 
-import org.springframework.context.event.EventListener;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class Inventory {
 
-    @EventListener
-    void on(InventoryUpdatedEvent event) {
+    @ApplicationModuleListener
+    public void onInventoryUpdatedEvent(InventoryUpdatedEvent event) throws InterruptedException {
+        System.out.println("An inventory has been started...");
+        Thread.sleep(10_000);
         System.out.println("The inventory has been updated [" + event + "]");
     }
 }
--- src/main/java/dev/smo/spring/example/service/order/Orders.java
+++ src/main/java/dev/smo/spring/example/service/order/Orders.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import java.util.Set;
 public class Orders {
 }
 
+@Transactional
 @RestController
 class OrdersController {
 
